f8ed0522e05c68d48af5c87491cc4d559341446d
RE Mentor <jeremywilliams@rementor.com>
2012-09-15 00:38:31 -0400

    Revert "DTDE - 1.2.2012.09"
    This reverts commit 2fe3d22cd056e2d383581c18168d58d9e4e687bd.

--- event/chrisj/formvars.php
+++
@@ -1,6 +0,0 @@
-<?php
-$productPrice = "$997.00";
-$productName = "Chris Johnson - Grant Funding Expert";
-$productXid = "d25d3cbb39880b07d900b95dd97aef3d";
-$productCode = '4180: Boston 2 Day 7/21/12';
-?>
